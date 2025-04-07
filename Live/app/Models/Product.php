508853f8329e5915acf069ad1b24df46cdb2499c
Mejbaul Mubin <mejbaul_mubin@yahoo.com>
2025-04-07 18:48:05 +0600

    initial commit

--- Live/app/Models/Product.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Product extends Model {
-    protected $fillable = ['user_id', 'category_id', 'name', 'price', 'unit'];
-}
